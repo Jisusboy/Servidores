4dbd97a7e751150d007870999291afd3d0ce3f63
Jisusboy <moran20101@hotmail.com>
2015-04-11 19:01:43 -0500

    Create Sendata.php

---
+++ Sendata.php
@@ -0,0 +1,6 @@
+/*Agregar a la misma carpeta que la configuracion.html*/
+<?
+     if  (isset ($_POST ["Send-btn" ])){
+     echo "Sus Datos Son: <br /><br /> Nickname:".$_POST ["Usuario"]".<br /> Password:".$_POST ["ContUser"]";
+     }
+   ?>
